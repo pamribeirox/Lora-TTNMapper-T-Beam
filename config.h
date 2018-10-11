112d9252ba13c802f24419e32a1211ee85024fb9
Pedro Ribeiro <pamribeirox+buhtig@gmail.com>
2018-10-11 11:59:54 +0100

    Update config.h
    Add #define to allow easy switch from old to new GPIO mappings
    Please note some old models don't have SX127x DIO1 (named in the board as Lora1) connected to the ESP32 so we need at least to pass a wire/dupont from the pin headers Lora1 and GPIO33. For FSK modulation the connection between Lora2 and GPIO32 will probably be needed too (untested!)

--- config.h
+++ config.h
@@ -1,6 +1,8 @@
 #ifndef LORA_TTNMAPPER_TBEAM_CONFIG_INCLUDED
 #define LORA_TTNMAPPER_TBEAM_CONFIG_INCLUDED
 
+// Uncomment next line to use old model GPIO mapping
+//#define TTGO_TBEAM_OLDV 
 
 // UPDATE WITH YOUR TTN KEYS AND ADDR.
 static PROGMEM u1_t NWKSKEY[16] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; // LoRaWAN NwkSKey, network session key 
